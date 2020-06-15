318b9dd982f96a33763b035d2cb2985bf3885662
mgeri93 <48027734+mgeri93@users.noreply.github.com>
2020-06-15 15:15:40 +0200

    Add files via upload

---
+++ src/com/zetcode/Board.java
@@ -0,0 +1,498 @@
+/* package com.zetcode;  //UtilityTimer
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Board extends JPanel  {
+
+    private final int B_WIDTH = 350;
+    private final int B_HEIGHT = 350;
+    private final int INITIAL_X = -40;
+    private final int INITIAL_Y = -40;
+    private final int INITIAL_DELAY = 100;
+    private final int PERIOD_INTERVAL = 25;
+
+    private Image star;
+    private Timer timer;
+    private int x, y;
+
+    public Board() {
+
+        initBoard();
+    }
+
+    private void loadImage() {
+
+        ImageIcon ii = new ImageIcon("src/resources/star.png");
+        star = ii.getImage();
+    }
+
+    private void initBoard() {
+
+        setBackground(Color.BLACK);
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+        loadImage();
+
+        x = INITIAL_X;
+        y = INITIAL_Y;
+
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new ScheduleTask(),
+                INITIAL_DELAY, PERIOD_INTERVAL);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        drawStar(g);
+    }
+
+    private void drawStar(Graphics g) {
+
+        g.drawImage(star, x, y, this);
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private class ScheduleTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+            x += 1;
+            y += 1;
+
+            if (y > B_HEIGHT) {
+                y = INITIAL_Y;
+                x = INITIAL_X;
+            }
+
+            repaint();
+        }
+    }
+} */
+
+/* package com.zetcode; //Thread animation
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Board extends JPanel
+        implements Runnable {
+
+    private final int B_WIDTH = 350;
+    private final int B_HEIGHT = 350;
+    private final int INITIAL_X = -40;
+    private final int INITIAL_Y = -40;
+    private final int DELAY = 25;
+
+    private Image star;
+    private Thread animator;
+    private int x, y;
+
+    public Board() {
+
+        initBoard();
+    }
+
+    private void loadImage() {
+
+        ImageIcon ii = new ImageIcon("src/resources/star.png");
+        star = ii.getImage();
+    }
+
+    private void initBoard() {
+
+        setBackground(Color.BLACK);
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+        loadImage();
+
+        x = INITIAL_X;
+        y = INITIAL_Y;
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+
+        animator = new Thread(this);
+        animator.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        drawStar(g);
+    }
+
+    private void drawStar(Graphics g) {
+
+        g.drawImage(star, x, y, this);
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void cycle() {
+
+        x += 1;
+        y += 1;
+
+        if (y > B_HEIGHT) {
+
+            y = INITIAL_Y;
+            x = INITIAL_X;
+        }
+    }
+
+    @Override
+    public void run() {
+
+        long beforeTime, timeDiff, sleep;
+
+        beforeTime = System.currentTimeMillis();
+
+        while (true) {
+
+            cycle();
+            repaint();
+
+            timeDiff = System.currentTimeMillis() - beforeTime;
+            sleep = DELAY - timeDiff;
+
+            if (sleep < 0) {
+                sleep = 2;
+            }
+
+            try {
+                Thread.sleep(sleep);
+            } catch (InterruptedException e) {
+
+                String msg = String.format("Thread interrupted: %s", e.getMessage());
+
+                JOptionPane.showMessageDialog(this, msg, "Error",
+                        JOptionPane.ERROR_MESSAGE);
+            }
+
+            beforeTime = System.currentTimeMillis();
+        }
+    }
+} */
+//MovingSpriteEx
+/* package com.zetcode; //MovingSpriteEx
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+public class Board extends JPanel implements ActionListener {
+
+    private Timer timer;
+    private SpaceShip spaceShip;
+    private final int DELAY = 10;
+
+    public Board() {
+
+        initBoard();
+    }
+
+    private void initBoard() {
+
+        addKeyListener(new TAdapter());
+        setBackground(Color.black);
+        setFocusable(true);
+
+        spaceShip = new SpaceShip();
+
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        Graphics2D g2d = (Graphics2D) g;
+
+        g2d.drawImage(spaceShip.getImage(), spaceShip.getX(), spaceShip.getY(), this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        step();
+    }
+
+    private void step() {
+
+        spaceShip.move();
+
+        repaint(spaceShip.getX()-1, spaceShip.getY()-1, spaceShip.getWidth()+2, spaceShip.getHeight()+2);
+    }
+
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            spaceShip.keyReleased(e);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            spaceShip.keyPressed(e);
+        }
+    }
+} */
+
+package com.zetcode;
+
+import org.w3c.dom.css.Rect;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Board extends JPanel implements ActionListener {
+
+    private final int ICRAFT_X = 40;
+    private final int ICRAFT_Y = 60;
+    private final int DELAY = 15;
+    private Timer timer;
+    private SpaceShip spaceShip;
+    private List<Alien> aliens;
+    private boolean ingame;
+    private final int B_WIDTH = 400;
+    private final int B_HEIGHT = 300;
+
+    private final int[][] pos = {
+            {2380, 29}, {2500, 59}, {1380, 89},
+            {780, 109}, {580, 139}, {680, 239},
+            {790, 259}, {760, 50}, {790, 150},
+            {980, 209}, {560, 45}, {510, 70},
+            {930, 159}, {590, 80}, {530, 60},
+            {940, 59}, {990, 30}, {920, 200},
+            {900, 259}, {660, 50}, {540, 90},
+            {810, 220}, {860, 20}, {740, 180},
+            {820, 128}, {490, 170}, {700, 30}
+    };
+
+    public Board(){
+
+        initBoard();
+    }
+
+    private void initBoard() {
+
+        addKeyListener(new TAdapter());
+        setBackground(Color.BLACK);
+        setFocusable(true);
+        ingame = true;
+
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+
+        spaceShip = new SpaceShip(ICRAFT_X, ICRAFT_Y);
+
+        initAliens();
+
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    public void initAliens() {
+        aliens = new ArrayList<>();
+
+        for (int[] p : pos) {
+            aliens.add(new Alien(p[0], p[1]));
+        }
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        if (ingame) {
+
+            drawObjects(g);
+
+        } else {
+
+            drawGameOver(g);
+        }
+
+        Toolkit.getDefaultToolkit().sync();
+    }
+
+    private void drawObjects(Graphics g) {
+
+        if (spaceShip.isVisible()) {
+            g.drawImage(spaceShip.getImage(), spaceShip.getX(), spaceShip.getY(), this);
+        }
+
+        List<Missile> ms = spaceShip.getMissiles();
+
+        for (Missile missile : ms) {
+            if (missile.isVisible()) {
+                g.drawImage(missile.getImage(), missile.getX(), missile.getY(), this);
+            }
+        }
+
+        for (Alien alien : aliens) {
+            if (alien.isVisible()) {
+                g.drawImage(alien.getImage(), alien.getX(), alien.getY(), this);
+            }
+        }
+
+        g.setColor(Color.WHITE);
+        g.drawString("Aliens left: " + aliens.size(), 5, 15);
+    }
+
+    private void drawGameOver(Graphics g) {
+
+        String msg = "Game Over";
+        Font small = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics fm = getFontMetrics(small);
+
+        g.setColor(Color.white);
+        g.setFont(small);
+        g.drawString(msg, (B_WIDTH - fm.stringWidth(msg)) / 2, B_HEIGHT /2);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        inGame();
+
+        updateAliens();
+        updateMissiles();
+        updateShip();
+
+        checkCollisions();
+
+        repaint();
+    }
+
+    private void inGame() {
+
+        if (!ingame) {
+            timer.stop();
+        }
+    }
+
+    private void updateShip() {
+
+        if (spaceShip.isVisible()) {
+
+            spaceShip.move();
+        }
+    }
+
+    private void updateMissiles() {
+
+        List<Missile> ms = spaceShip.getMissiles();
+
+        for (int i = 0; i < ms.size(); i++) {
+
+            Missile m = ms.get(i);
+
+            if (m.isVisible()) {
+                m.move();
+            } else {
+                ms.remove(i);
+            }
+        }
+    }
+
+    private void updateAliens() {
+
+        if (aliens.isEmpty()) {
+
+            ingame = false;
+            return;
+        }
+
+        for (int i = 0; i < aliens.size(); i++) {
+
+            Alien a = aliens.get(i);
+
+            if (a.isVisible()) {
+                a.move();
+            } else {
+                aliens.remove(i);
+            }
+        }
+    }
+
+    public void checkCollisions() {
+
+        Rectangle r3 = spaceShip.getBounds();
+
+        for (Alien alien : aliens) {
+
+            Rectangle r2 = alien.getBounds();
+            if (r3.intersects(r2)) {
+
+                spaceShip.setVisible(false);
+                alien.setVisible(false);
+                ingame = false;
+            }
+        }
+
+        List<Missile> ms = spaceShip.getMissiles();
+
+        for (Missile m : ms) {
+
+            Rectangle r1 = m.getBounds();
+
+            for (Alien alien : aliens) {
+
+                Rectangle r2 = alien.getBounds();
+
+                if (r1.intersects(r2)) {
+
+                    m.setVisible(false);
+                    alien.setVisible(false);
+                }
+            }
+        }
+    }
+
+    private class TAdapter extends KeyAdapter {
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            spaceShip.keyReleased(e);
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            spaceShip.keyPressed(e);
+        }
+    }
+}
